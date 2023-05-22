c36e6e43cd1add2dcc241ee9589e5b83910db7c5
DimasCW <cdimas292@gmail.com>
2023-05-22 15:44:28 +0700

    membuat class ibu

--- mainIA/ibu.h
+++ mainIA/ibu.h
@@ -1,1 +1,20 @@
 #pragma once
+#ifndef IBU_H
+#define IBU_H
+
+class ibu {
+public:
+	string nama:
+	vector<anak*> daftar_anak;
+
+	ibu(string pnama) :(nama(pnama) {
+		cout << "ibu \"" << nama << "\" ada\n";
+	}
+	~ibu(string pnama) :(nama(pnama) {
+		cout << "ibu \"" << nama << "\"tidak ada\n";
+	}
+
+
+
+};
+#endif
