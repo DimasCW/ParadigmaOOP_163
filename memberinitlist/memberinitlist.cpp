761b3dc38283a949e564ab519e25c124300aaaf5
DimasCW <cdimas292@gmail.com>
2023-05-22 15:22:41 +0700

    membuat class mahasiswa

--- memberinitlist/memberinitlist.cpp
+++ memberinitlist/memberinitlist.cpp
@@ -5,4 +5,16 @@ using namespace std;
 class mahasiswa {
 private:
 	const int id;
+	string nama;
+	float nilai;
+
+public:
+	mahasiswa(int pid, string pnama, float pnilai) :id(pid), nama(pnama), nilai(pnilai) {
+		//definisi
+	}
+	~mahasiswa() {
+		cout << "id " << id << endl;
+		cout << "nama " << nama << endl;
+		cout << "nilai " << nilai << endl;
+	}
 };
