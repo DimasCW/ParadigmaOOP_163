238a10331e4e6987b4a84bdf54f8d4d8644f1e63
DimasCW <cdimas292@gmail.com>
2023-05-22 15:59:15 +0700

    membuat int main

--- mainIA/mainIA.cpp
+++ mainIA/mainIA.cpp
@@ -1,8 +1,15 @@
-#include<iostream>
+#include<iostream>;
+#include<vector>
 #include"anak.h"
 #include"ibu.h"
 using namespace std;
 
 int main(){
+	ibu* varibu = new ibu("");
+	ibu* varibu2 = new ibu("");
+	anak* varanak1 = new anak("");
+	anak* varanak2 = new anak("");
+	anak* varanak3 = new anak("");
+
 
 }
