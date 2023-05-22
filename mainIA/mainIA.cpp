6d5613e25b9e0debc93d8f6b6984a2038125916b
DimasCW <cdimas292@gmail.com>
2023-05-22 16:03:47 +0700

    menyelesaikan file mainia

--- mainIA/mainIA.cpp
+++ mainIA/mainIA.cpp
@@ -5,11 +5,25 @@
 using namespace std;
 
 int main(){
-	ibu* varibu = new ibu("");
-	ibu* varibu2 = new ibu("");
-	anak* varanak1 = new anak("");
-	anak* varanak2 = new anak("");
-	anak* varanak3 = new anak("");
+	ibu* varibu = new ibu("dini");
+	ibu* varibu2 = new ibu("novi");
+	anak* varanak1 = new anak("tono");
+	anak* varanak2 = new anak("rini");
+	anak* varanak3 = new anak("dewi");
 
+	varibu->tambahanak(varanak1);
+	varibu->tambahanak(varanak2);
+	varibu2->tambahanak(varanak3);
+	varibu2->tambahanak(varanak1);
 
+	varibu->cetakanak();
+	varibu2->cetakanak();
+
+	delete varibu;
+	delete varibu2;
+	delete varanak1;
+	delete varanak2;
+	delete varanak3;
+
+	return 0;
 }
